e0b63cd17d58ef2241874ee831f1b0bf6e4b0406
emanuelerm <emanueler00@gmail.com>
2023-05-17 14:09:06 +0200

    scaffolding

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche:
+- L'e-commerce vende *prodotti* per animali.
+- I prodotti sono categorizzati, le *categorie* sono Cani o Gatti.
+- I prodotti saranno oltre al *cibo*, anche *giochi*, *cucce*, etc.
+Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, prezzo, icona della categoria ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia).
+*/
+
+include __DIR__ . '/Views/header.php';
+include __DIR__ . '/Views/main.php';
+include __DIR__ . '/Views/footer.php';
+
